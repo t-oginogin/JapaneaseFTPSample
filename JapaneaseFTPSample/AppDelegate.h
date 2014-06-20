ea4583869fd149c652d7a270e519d9f4cf17e2d0
Tatsuya Ogi <t.oginogin@gmail.com>
2014-06-21 06:32:55 +0900

    Initial Commit

---
+++ JapaneaseFTPSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JapaneaseFTPSample
+//
+//  Created by Tatsuya Ogi on 2014/06/21.
+//  Copyright (c) 2014年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JapaneaseFTPSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JapaneaseFTPSample
+//
+//  Created by Tatsuya Ogi on 2014/06/21.
+//  Copyright (c) 2014年 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
